f8e19c58cd7c908e0c6d85d4e5cdcb0f3983a498
srgp2 <srgp2@kent.ac.uk>
2025-01-19 00:28:29 +0000

    fetch runtime for films

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Styles from './components/styles.css';
 import PopularFilms from './services/popularFilms.js';
+import Runtime from './services/runtime.js';
 //API KEY: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTY3N2NmYzE0Mzk2ODBmMzc1YTQwN2Q0ODU1ZjA3ZiIsIm5iZiI6MTczNjY4Mzg0OS4yNDUsInN1YiI6IjY3ODNiMTQ5YmQ3OTNjMDM1NDRlOTRiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QbyteWIRgysb7ffZbt390rpbwAU_Zt3m4qYlPKV2sao
 
 
 function App() {
     return (
     <div>
-     <Header />
+     <Header /> 
       <main>
         <h2>Welcome to My Website</h2>
         <p>React makes building UI easy!</p>
         <PopularFilms />
+        <Runtime />
       </main>
     </div>
     
--- src/services/runtime.js
+++ src/services/runtime.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import PopularFilms from './popularFilms.js';
 
-function popularFilms() {
-    const [data, setData] = useState(null); // State to store API data
-    const [loading, setLoading] = useState(true); // State to track loading state
-    const [error, setError] = useState(null); // State to track errors
-    const [title, setTitle] = useState(null);
 
+console.log("loading")
+function runtime() {
+    const [data, setData] = useState(null);
+    const [length, setLength] = useState(null);
+
+    console.log("Runtime.js is loaded!");
     useEffect(() => {
         const options = {
             method: 'GET',
@@ -14,31 +16,30 @@ function popularFilms() {
                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTY3N2NmYzE0Mzk2ODBmMzc1YTQwN2Q0ODU1ZjA3ZiIsIm5iZiI6MTczNjY4Mzg0OS4yNDUsInN1YiI6IjY3ODNiMTQ5YmQ3OTNjMDM1NDRlOTRiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QbyteWIRgysb7ffZbt390rpbwAU_Zt3m4qYlPKV2sao'
             }
         };
-        fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+        const baseUrl = 'https://api.themoviedb.org/3/movie/'
+        const id = localStorage.getItem("selectedMovieId")
+        var fullUrl = baseUrl + id + '?language=en-US'
+        fetch(fullUrl, options)
         .then((response) => {
             if (!response.ok) {
-                throw new Error("Failed to fetch data");
+                throw new Error("Failed to fetch data"); 
             }
-            
             return response.json();
         })
         .then((data) => {
-            setData(data)
-            setLoading(false)
-            setTitle(data.results[0].original_title)
-            localStorage.setItem("selectedMovieId", data.results[0].id);
             console.log(data)
+            console.log("hitting")
+            setLength(data.runtime)
         })
         .catch((error) => {
             setError(error.message)
             setLoading(false)
         })
     }, [])
-
     return(
         <div>
-            <p>{title}</p>
-        </div>
+            <p>{length}</p>
+        </div> 
     )
 }
-export default popularFilms;
+export default runtime;
