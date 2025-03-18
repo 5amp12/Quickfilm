08e3b831a7bf4ebe99c00fea64c30e4d11d5b8e0
5amp12 <samuel.passey@hotmail.com>
2025-03-18 20:01:04 +0000

    Improving style and functionality of SignUp

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 const pool = require("../config/db");
 const bcrypt = require("bcrypt");
 
+ //WHEN EDITING HERE MAKE SURE TO RELOAD NODE SERVER.JS COMMAND TO SEE EFFECTS
+
 exports.signUp = async (req, res) => {            //req means the request from the user  and res is used for the response to the user
     const {username, password} = req.body;
     try{
@@ -15,8 +17,16 @@ exports.signUp = async (req, res) => {            //req means the request from t
         );
 
         res.status(201).json({ message: "User registered successfully", user: newUser.rows[0] });
-    }catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal server error" });
+    }catch (error) {        
+        console.error("error :", error);
+        // console.error("Error Message :", error.message);
+
+        if (error.code === '23505') {     //error number for unique constraint violation in postgres
+            return res.status(400).json({ error: "Username already in use" });
+        }
+        
+        return res.status(500).json({ error: "Internal server error" });
+        
+        
     }
 }
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,26 +1,34 @@
 import { useEffect, useState } from "react";
 import { signUpUser } from "../../services/authService";
+import "./SignUp.css";
 
 
 function SignUp() {
     const [username, setUsername ] = useState ("");
     const [password, setPassword] = useState ("");
+    const [confirmPassword, setConfirmPassword] = useState ("");
     const [message, setMessage] = useState ("");
 
     const handleSubmit = async (e) => {
         e.preventDefault();  //prevents page from reloading when the form is being submitted
 
-        const result = await signUpUser(username, password);
+        if (confirmPassword == password){
 
-        if (result.error){
-            setMessage(result.error);
-        } else {
-            setMessage("Registration Successful");
+            const result = await signUpUser(username, password);
+
+            if (result.error){
+                setMessage(result.error);
+            } else {
+                setMessage("Registration Successful");
+            }
+        }
+        else{
+            setMessage("Passwords do not match")
         }
     };
 
     return(
-        <form onSubmit={handleSubmit}>
+        <form className="signup-form" onSubmit={handleSubmit}>
             <input 
                 placeholder="username..."
                 value={username} 
@@ -33,9 +41,14 @@ function SignUp() {
                 onChange={(e) => setPassword(e.target.value)} 
                 required>
             </input>
-            
+            <input
+                placeholder="confirm password..." 
+                value={confirmPassword} 
+                onChange={(e) => setConfirmPassword(e.target.value)} 
+                required>
+            </input>
             <button type="submit">Submit</button>
-            {message && <p>{message}</p>}
+            {message && <p className="error-message">{message}</p>}
         </form>
     )
 }
