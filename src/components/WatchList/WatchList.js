513584adbdacb3f2bfb61eb868b4e0618310ecb2
5amp12 <samuel.passey@hotmail.com>
2025-08-16 20:07:50 +0100

    Starting on watchlist page

--- src/components/WatchList/WatchList.js
+++ src/components/WatchList/WatchList.js
@@ -1,7 +1,27 @@
-
-
+import { useEffect, useState, useRef } from "react";
+import { checkWatchList } from "../../services/authService";
+import { fetchFilmId } from "../../services/api"
 
 function WatchList() {
     console.log("hello")
+    
+   
+    const scanWatchList = async () => {
+        try{
+            const filmsID = await checkWatchList();
+            console.log(filmsID);
+            for (let film of filmsID.watchlist){
+                
+                const data = await fetchFilmId(film);
+                console.log(data);
+            }
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+    scanWatchList();
+
+    
 }
 export default WatchList;
