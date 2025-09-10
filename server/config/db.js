ca1886e1a990997fd20acaacd74a2e579508de9b
5amp12 <samuel.passey@hotmail.com>
2025-09-10 18:54:56 +0100

    switching back to using URL

--- server/config/db.js
+++ server/config/db.js
@@ -10,16 +10,16 @@ if (process.env.SUPABASE_DB_URL) {
     ssl: { rejectUnauthorized: false }, 
   });
   console.log("DB: using SUPABASE_DB_URL");
-} else if (process.env.SUPABASE_DB_HOST) {
-  pool = new Pool({
-    host: process.env.SUPABASE_DB_HOST,
-    port: process.env.SUPABASE_DB_PORT,
-    user: process.env.SUPABASE_DB_USER,
-    database: process.env.SUPABASE_DB_NAME,
-    password: process.env.SUPABASE_DB_PASSWORD,
-    ssl: { rejectUnauthorized: false },
-  });
-  console.log("DB: using SUPABASE_* parts (host/port/user/db)");
+// } else if (process.env.SUPABASE_DB_HOST) {
+//   pool = new Pool({
+//     host: process.env.SUPABASE_DB_HOST,
+//     port: process.env.SUPABASE_DB_PORT,
+//     user: process.env.SUPABASE_DB_USER,
+//     database: process.env.SUPABASE_DB_NAME,
+//     password: process.env.SUPABASE_DB_PASSWORD,
+//     ssl: { rejectUnauthorized: false },
+//   });
+//   console.log("DB: using SUPABASE_* parts (host/port/user/db)");
 } else {
   // Otherwise, fall back to local dev settings
   pool = new Pool({
